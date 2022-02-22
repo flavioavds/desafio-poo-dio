9817766eda902601f3338d9dc540d2bc0453ee0e
Flavio Augusto <favds123@gmail.com>
2022-02-22 12:16:03 -0300

    final

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -21,13 +21,43 @@ public class Main {
 		mentoria.setDescricao("descrição mentoria java");
 		mentoria.setData(LocalDate.now());
 		
-		System.out.println(curso1);
-		System.out.println(curso2);
+//		System.out.println(curso1);
+//		System.out.println(curso2);		
+//		System.out.println(mentoria);
 		
-		System.out.println(mentoria);
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescrcao("Descrição Bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
 		
+		Dev devFlavio = new Dev();
+		devFlavio.setNome("Flavio");
+		devFlavio.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos Inscritos Flavio " + devFlavio.getConteudoInscritos());
 		
+		devFlavio.progredir();
+		devFlavio.progredir();
+		System.out.println("-");
+		System.out.println("Conteudos Inscritos Flavio " + devFlavio.getConteudoInscritos());
+		System.out.println("Conteudos Concluidos Flavio " + devFlavio.getConteudoConcluidos());
+		System.out.println("XP: " + devFlavio.calcularXp());
 		
+		System.out.println("---------");
+		
+		Dev devCamila = new Dev();
+		devCamila.setNome("Camila");
+		devCamila.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos Inscritos Camila " + devCamila.getConteudoInscritos());
+		
+		devCamila.progredir();
+		devCamila.progredir();
+		devCamila.progredir();
+		System.out.println("-");
+		System.out.println("Conteudos Inscritos Camila " + devCamila.getConteudoInscritos());
+		System.out.println("Conteudos Concluidos Camila " + devCamila.getConteudoConcluidos());
+		System.out.println("XP: " + devCamila.calcularXp());
 		
 
 	}
